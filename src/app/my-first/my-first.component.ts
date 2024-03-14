91b947cd15fb4328153aa3b251b5acf95e9ac925
Moribo Mdluli <moribomdluli@gmail.com>
2024-03-14 22:33:34 +0200

    first commit

---
+++ src/app/my-first/my-first.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first',
+  templateUrl: './my-first.component.html',
+  styleUrls: ['./my-first.component.scss']
+})
+export class MyFirstComponent {
+  name: string = " ";
+  email: string = " ";
+  message: string = " ";
+
+}
